5694ef791397ddb1aea6c45b8a2d7cf76149a7b8
johnb8005 <johan@nexys.ch>
2023-05-28 12:52:55 +0200

    fix company insert

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMzNg/spectral.js?branch_slug=master&token=5ab4f561-eead-44d0-a2df-f08bddda8302";
+export default { extends: ruleset };
